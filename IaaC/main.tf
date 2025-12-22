751a927eabccaf5aa450d862376d9fc162e17e6c
lakshmi <lakshmichennur22@gmail.com>
2025-12-22 14:12:08 +0530

    main file modified

--- IaaC/main.tf
+++ IaaC/main.tf
@@ -3,7 +3,7 @@
 # -----------------------
 locals {
   name_prefix = "${var.project}-${var.application}-${var.environment}-${var.location_short}"
-  rg_name     = "${local.name_prefix}-rg"
+
   tags = {
     project     = var.project
     application = var.application
@@ -13,129 +13,44 @@ locals {
   }
 }
 
-# ---------------------------
-# RESOURCE GROUP
-# ---------------------------
-resource "azurerm_resource_group" "rg" {
-  name     = local.rg_name
-  location = var.location
-  tags     = local.tags
+resource "aws_s3_bucket" "frontend" {
+  bucket = "${local.name_prefix}-s3-bucket"
 }
 
-# ---------------------------
-# VNET + SUBNET
-# ---------------------------
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${local.name_prefix}-vnet"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  address_space       = ["10.0.0.0/16"]
-  tags                = local.tags
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "${local.name_prefix}-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
 
-# ---------------------------
-# PUBLIC IP (one per VM)
-# ---------------------------
-resource "azurerm_public_ip" "pip" {
-  count               = 3
-  name                = "${local.name_prefix}-pip-${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
+  index_document {
+    suffix = "index.html"
+  }
 
-# ---------------------------
-# NIC (one per VM)
-# ---------------------------
-resource "azurerm_network_interface" "nic" {
-  count               = 3
-  name                = "${local.name_prefix}-nic-${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip[count.index].id
+  error_document {
+    key = "index.html"
   }
 }
 
-# ---------------------------
-# LINUX VMs (3)
-# ---------------------------
-resource "azurerm_linux_virtual_machine" "vm" {
-  count                           = 3
-  name                            = "${local.name_prefix}-vm-${count.index}"
-  location                        = azurerm_resource_group.rg.location
-  resource_group_name             = azurerm_resource_group.rg.name
-  size                            = "Standard_F2"
-  admin_username                  = "azureuser"
-  disable_password_authentication = false
-  admin_password                  = var.admin_password
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
 
-  custom_data = filebase64("cloud-init-script.sh")
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-  network_interface_ids = [
-    azurerm_network_interface.nic[count.index].id,
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.frontend.arn}/*"
+    }]
+  })
+  depends_on = [
+    aws_s3_bucket_public_access_block.frontend
   ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-}
-
-# ---------------------------
-# NSG
-# ---------------------------
-resource "azurerm_network_security_group" "nsg" {
-  name                = "${local.name_prefix}-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "AllowHTTP"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "AllowSSHFromAny"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-# Attach NSG to subnet
-resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
-  subnet_id                 = azurerm_subnet.subnet.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
 }
