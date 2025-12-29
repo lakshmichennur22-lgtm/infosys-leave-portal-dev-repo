b7bbfbcd5d3b81e284b34c43b779de858308c821
lakshmi <lakshmichennur22@gmail.com>
2025-12-29 15:54:07 +0530

    infra

--- IaaC/outputs.tf
+++ IaaC/outputs.tf
@@ -1,3 +1,3 @@
-output "frontend_website_url" {
-  value = "http://${aws_s3_bucket.frontend.bucket}.s3-website-${var.location}.amazonaws.com"
+output "frontend_url" {
+  value = aws_lb.frontend_alb.dns_name
 }
